4059c4e18a70ecb251b36310ee63b5b5c5b3c02c
Ludivik Eduardo Silva de Paula <65737633+Ldvk-bf@users.noreply.github.com>
2025-10-22 11:23:13 -0300

    Refactor body color change logic and messaging
    Simplifies bodyColorChange by toggling document.body.id directly. Updates canalColor.onmessage to handle requestColorState and send updated nextID with requestColorState reset to false.

--- js/change_color_operator.js
+++ js/change_color_operator.js
@@ -8,9 +8,8 @@ function changeColor() {
 }
 
 function bodyColorChange() {
-    const elemento = document.getElementById("bodyException");
-    if (elemento) {
-        elemento.id = " ";
+    if (document.body.id == "bodyException") {
+        document.body.id = " ";
     } else {
         document.body.id = "bodyException";
     }
@@ -20,5 +19,15 @@ canalColor.onmessage = (event) => {
     const requestColorState = event.data.requestColorState;
     console.log("Color changed in message:", requestColorState);
 
-    canalColor.postMessage({ nextID });
+    if (requestColorState) {
+        if (document.body.id == " ") {
+            nextID = "bodyException";
+        } else {
+            nextID = " ";
+        }
+        canalColor.postMessage({ nextID, requestColorState: false });
+    }
+    
+
+    
 };
